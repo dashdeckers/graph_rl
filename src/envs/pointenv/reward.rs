9dd6737946204ec26d7328a979a288f3709fac91
Travis Hammond <travis.hammond@alexanderthamm.com>
2023-11-08 13:04:53 +0100

    add SoRB distance reward

--- src/envs/pointenv/reward.rs
+++ src/envs/pointenv/reward.rs
@@ -4,6 +4,7 @@ use super::super::DistanceMeasure;
 #[derive(Debug)]
 pub enum PointReward {
     Euclidean,
+    Distance,
     Sparse,
     SparseTimePenalty,
 }
@@ -15,6 +16,7 @@ impl PointReward {
     ) -> f64 {
         match self {
             PointReward::Euclidean => -PointState::distance(state, goal),
+            PointReward::Distance => -1.0,
             PointReward::Sparse => if state == goal {1.0} else {0.0},
             PointReward::SparseTimePenalty => if state == goal {1.0} else {-1.0},
         }
