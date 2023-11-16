70ee7ff226652a6d65be8fecc6c3b7d9ab07b92b
Travis Hammond <dashdeckers@gmail.com>
2023-11-16 13:38:40 +0100

    handle multiple parquet files

--- viz_data.py
+++ viz_data.py
@@ -2,8 +2,22 @@ import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from pathlib import Path
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-d', "--dir",)
+args = parser.parse_args()
+if args.dir is None:
+    print("No path provided")
+    sys.exit()
+
+path = Path(args.dir)
+
 df = (
-    pl.read_parquet("pendulum_data.parquet")
+    pl.read_parquet(path / "run_*_data.parquet")
     .with_columns(
         pl.concat_list("^run_.*_total_rewards$").list.eval(
             pl.element().std()
@@ -30,6 +44,7 @@ plt.fill_between(
 )
 plt.xlabel("Episode")
 plt.ylabel("Total reward")
-plt.title("Learning Curve for DDPG on Pendulum-v0")
+plt.title("Learning Curve")
 
-plt.show()
+plt.savefig(path / "plot.png")
+print("Success")
