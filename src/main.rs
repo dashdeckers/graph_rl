9a0ee693cc86a706ffa04c3cbf4e0f36e823155f
Travis Hammond <dashdeckers@gmail.com>
2023-11-24 20:15:26 +0100

    no need to log to stdout

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ use {
 pub fn setup_logging(
     path: &dyn AsRef<Path>,
     min_level_file: Option<Level>,
-    min_level_stdout: Option<Level>,
+    // min_level_stdout: Option<Level>,
 ) -> Result<()> {
     let path = Path::new("data/").join(path);
     create_dir_all(path.as_path())?;
@@ -82,19 +82,19 @@ pub fn setup_logging(
                 }))
                 .with_ansi(false),
         )
-        // Stdout writer
-        .with(
-            layer()
-                .with_writer(std::io::stdout.with_max_level(match min_level_stdout {
-                    Some(level) => level,
-                    None => Level::INFO,
-                }))
-                .compact()
-                .pretty()
-                .with_line_number(true)
-                .with_thread_ids(false)
-                .with_target(false),
-        )
+        // // Stdout writer
+        // .with(
+        //     layer()
+        //         .with_writer(std::io::stdout.with_max_level(match min_level_stdout {
+        //             Some(level) => level,
+        //             None => Level::INFO,
+        //         }))
+        //         .compact()
+        //         .pretty()
+        //         .with_line_number(true)
+        //         .with_thread_ids(false)
+        //         .with_target(false),
+        // )
         // Create and set Subscriber
         .init();
 
@@ -193,7 +193,7 @@ where
     setup_logging(
         &name,
         args.log.level(),
-        args.log.level(),
+        // args.log.level(),
     )?;
 
     let device = if args.cpu {
