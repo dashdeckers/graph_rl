853468f614e2b002bafe41c15d7b9e7a7999290d
Travis Hammond <travis.hammond@alexanderthamm.com>
2024-04-28 19:40:13 +0200

    cleanup last script

--- scripts/run_both_algs.sh
+++
@@ -1,5 +0,0 @@
-size='small'
-name='tensor-true'
-
-nohup cargo run --release -- --env pointenv --alg ddpg --log warn --runs 100 --name "ddpg-$size-$name" &
-nohup cargo run --release -- --env pointenv --alg ddpg-sgm --log warn --runs 100 --name "ddpg-sgm-$size-$name" &
